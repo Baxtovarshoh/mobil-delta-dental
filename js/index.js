57ec7cfcc1a6a3f37f80199a01bcbf269c4ea001
Baxtovarshoh <boboevbaxtovarshoh@gmail.com>
2026-02-20 06:11:39 +0500

    34wde34

--- js/index.js
+++ js/index.js
@@ -1,47 +1,142 @@
-const track = document.querySelector(".track");
-const slides = document.querySelectorAll(".slide");
+const swiper = document.querySelector(".swiper");
+const dotsContainer = document.querySelector(".dots");
+let slides = document.querySelectorAll(".slide");
+const haederText = document.querySelector(".texts");
+const liElem = [
+  {
+    id: "0",
+    text: "Why Delta Dental",
+  },
+  {
+    id: "1",
+    text: "Plans for your employees",
+  },
+  {
+    id: "2",
+    text: "Small Business",
+  },
+  {
+    id: "3",
+    text: "Large Groups",
+  },
+  {
+    id: "4",
+    text: "Why Add DeltaVision®?",
+  },
+];
 
-let index = 1;
-let startX = 0;
-let isDragging = false;
+let index = 0;
+let slideWidth = slides[0].offsetWidth;
+window.addEventListener("resize", () => {
+  slideWidth = slides[0].offsetWidth;
+  console.log(slideWidth);
 
-const width = slides[0].offsetWidth + parseFloat(getComputedStyle(track).gap);
+  updateText();
+  moveTo(index);
+});
 
-function update(animate = true) {
-  track.style.transition = animate ? "transform 0.35s ease" : "none";
-  track.style.transform = `translateX(-${index * width}px);`
+liElem.forEach((value, i) => {
+  let li = document.createElement("li");
+  li.classList.add("atr-list");
+  li.id = value.id;
+  li.textContent = value.text;
+  haederText.appendChild(li);
+  updateText();
+});
+
+function updateText() {
+  const text = document.querySelectorAll(".atr-list");
+  text.forEach((value, i) => {
+    if (i === index) {
+      value.classList.add("active");
+    } else {
+      value.classList.remove("active");
+    }
+  });
 }
 
-track.addEventListener("touchstart", (e) => {
-  startX = e.touches[0].clientX;
+createDots();
+moveTo(0, false);
+
+let startX = 0;
+let currentX = 0;
+let isDragging = false;
+let isSwipe = false;
+
+swiper.addEventListener("pointerdown", (e) => {
+  startX = e.clientX;
   isDragging = true;
+  isSwipe = false;
+  swiper.style.transition = "none";
 });
 
-track.addEventListener("touchend", (e) => {
+swiper.addEventListener("pointermove", (e) => {
   if (!isDragging) return;
-  isDragging = false;
 
-  const diff = e.changedTouches[0].clientX - startX;
-
-  if (diff < -60) index++;
-  if (diff > 60) index--;
-
-  update();
-
-  track.addEventListener(
-    "transitionend",
-    () => {
-      if (index === slides.length - 1) {
-        index = 1;
-        update(false);
-      }
-      if (index === 0) {
-        index = slides.length - 2;
-        update(false);
-      }
-    },
-    { once: true },
-  );
+  currentX = e.clientX;
+  let diff = currentX - startX;
+  if (Math.abs(diff) > 5) {
+    isSwipe = true;
+    swiper.style.transform = `translateX(${-slideWidth * index + diff}px)`;
+  }
 });
 
-update(false);
+swiper.addEventListener("pointerup", () => {
+  if (!isDragging) return;
+
+  let diff = currentX - startX;
+  swiper.style.transition = ".4s";
+
+  if (!isSwipe) {
+    isDragging = false;
+    return;
+  }
+
+  if (diff > 60) moveTo(index - 1);
+  else if (diff < -60) moveTo(index + 1);
+  else moveTo(index);
+
+  isDragging = false;
+});
+
+swiper.addEventListener("pointerleave", () => {
+  isDragging = false;
+});
+
+function moveTo(i, smooth = true) {
+  if (i >= slides.length) i = 0;
+  if (i < 0) i = slides.length - 1;
+
+  swiper.style.transition = smooth ? ".4s" : "none";
+  swiper.style.transform = `translateX(-${slideWidth * i}px)`;
+
+  index = i;
+  updateText();
+  updateDots();
+}
+
+function next() {
+  moveTo(index + 1);
+}
+
+function createDots() {
+  dotsContainer.innerHTML = "";
+
+  for (let i = 0; i < slides.length; i++) {
+    const dot = document.createElement("div");
+    dot.classList.add("dot");
+
+    dot.onclick = () => moveTo(i);
+
+    dotsContainer.appendChild(dot);
+  }
+}
+
+function updateDots() {
+  const dots = document.querySelectorAll(".dot");
+  dots.forEach((d) => d.classList.remove("active"));
+
+  dots[index].classList.add("active");
+}
+
+// setInterval(next, 2500);
